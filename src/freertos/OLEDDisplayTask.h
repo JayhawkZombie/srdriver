d3bbc0760b365d7cc1c044744d62cb0d43d1c8cf
Kurt Slagle <kurt_slagle@yahoo.com>
2025-12-25 10:57:51 -0800

    Update OLEDDisplayTask.h

--- src/freertos/OLEDDisplayTask.h
+++ src/freertos/OLEDDisplayTask.h
@@ -91,47 +91,6 @@ private:
         BannerLine(Point _start, Point _end, Direction _direction) : start(_start), end(_end), direction(_direction) {}
     };
 
-    class AnimatedBorderLine {
-    public:
-        BannerLine _line;
-        int _fill = 0;
-        bool _isFilling = true;
-        int _advance = 2;
-
-        AnimatedBorderLine(const BannerLine& line) : _line(line) {}
-
-        void animate()
-        {
-            if (_isFilling)
-            {
-                _fill += _advance;
-            }
-            else
-            {
-                _fill -= _advance;
-            }
-        }
-
-        // void render()
-        // {
-        //     renderBannerLine(_line, _fill, _isFilling);
-        // }
-
-        bool isFinished()
-        {
-            if (_isFilling)
-            {
-                return _fill >= 127;
-            }
-        }
-
-        void reset()
-        {
-            _fill = 0;
-            _isFilling = true;
-        }
-    };
-
     BannerLine _bannerLines[4] = {
         { {0, 0}, {127, 0}, BannerLine::Direction::RIGHT },
         { {127, 0}, {127, 63}, BannerLine::Direction::DOWN },
