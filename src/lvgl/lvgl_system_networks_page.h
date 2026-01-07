5fa7db4e1a19dde7a92235030b010bf46f9dd2a7
Kurt Slagle <kurt_slagle@yahoo.com>
2026-01-07 12:06:00 -0800

    Skeleton for networks tab

--- src/lvgl/lvgl_system_networks_page.h
+++ src/lvgl/lvgl_system_networks_page.h
@@ -6,7 +6,9 @@
 /**
  * System Networks Settings Page
  * 
- * Creates the Networks settings page for the system menu
+ * Creates the Networks settings page for the system menu with:
+ * - Current network information (SSID, signal strength, IP address, status)
+ * - List of known/saved networks
  */
 
 /**
@@ -15,6 +17,12 @@
  * @return The created page object
  */
 lv_obj_t* createSystemNetworksPage(lv_obj_t* menu);
+
+/**
+ * Update the current network information display
+ * @param page The networks page object
+ */
+void updateSystemNetworksPage(lv_obj_t* page);
 
 #endif // PLATFORM_CROW_PANEL
 
