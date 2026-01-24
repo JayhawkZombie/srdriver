71977b11b83053c7e6c014dfad83d9eb327c6e40
Kurt Slagle <kurt_slagle@yahoo.com>
2026-01-24 15:37:26 -0800

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,13 +1,13 @@
 #pragma once
 
 // Auto-generated version information - DO NOT EDIT
-// Generated on 2026-01-17 at 09:59:09
-#define FIRMWARE_VERSION "dev-e5785c9-2026-01-17 09:59:09"
+// Generated on 2026-01-24 at 15:37:18
+#define FIRMWARE_VERSION "dev-94c2abf-2026-01-24 15:37:18"
 #define VERSION_TAG "dev"
-#define VERSION_HASH "e5785c9"
-#define VERSION_BRANCH "lvgl-system-page"
-#define BUILD_DATE "2026-01-17"
-#define BUILD_TIME "09:59:09"
-#define BUILD_TIMESTAMP "2026-01-17 09:59:09"
+#define VERSION_HASH "94c2abf"
+#define VERSION_BRANCH "cleanup"
+#define BUILD_DATE "2026-01-24"
+#define BUILD_TIME "15:37:18"
+#define BUILD_TIMESTAMP "2026-01-24 15:37:18"
 #define DEVICE_NAME "SRDriver"
 #define DEVICE_VERSION "hw_v0_02"
